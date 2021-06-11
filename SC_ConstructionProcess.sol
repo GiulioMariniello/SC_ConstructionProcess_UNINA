b2710c4b414bf44e254d1363fb70e4b8f417fa07
GiulioMariniello <68600379+GiulioMariniello@users.noreply.github.com>
2021-06-11 11:46:04 +0200

    Update SC_ConstructionProcess.sol

--- SC_ConstructionProcess.sol
+++ SC_ConstructionProcess.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.5;
+pragma solidity 0.5.5;
 contract Bcl {
 // Defining the structure Transmission
 struct Transmission{
@@ -12,16 +12,12 @@ bytes32 FileHash;
 bool Current_version;
 bytes32 FileHash_New;
 }
-bytes32 constant NULL = "";
+bytes32 constant public NULL = "";
 //Defining a array with the list of transmitted hashes 
 bytes32[] public ListdocHash;
-
-
 // Defining the structure map to store the docHashes in order to have 
 //an accesskey to the Transmission
-mapping (bytes32 => Transmission) private docHashes;
-
-
+mapping (bytes32 => Transmission) public docHashes;
 //Add transmission function
 function Add_transmission (bytes32 _FileName,bytes32 _FileType,
 bool _NewVersion, address _Recipient, bytes32 _FileHash, 
@@ -30,41 +26,36 @@ bytes32 _OldFileHash) public {
 if(_NewVersion == true) { // if else statement
 //Add new transmission
 Transmission memory newTransmission =Transmission (msg.sender,_Recipient,
-_FileName, _FileType,now, block.number, _FileHash, true, NULL);
+_FileName, _FileType,block.timestamp, block.number, _FileHash, true, NULL);
 docHashes[_FileHash] = newTransmission;
 ListdocHash.push(_FileHash);
-
 } else {
 //If it is a revision: Update the old version
 if (docHashes[_OldFileHash].Sender == msg.sender){
 docHashes[_OldFileHash].Current_version = false;
 docHashes[_OldFileHash].FileHash_New = _FileHash;
 Transmission memory newTransmission =Transmission (msg.sender,_Recipient, 
-_FileName, _FileType, now, block.number,_FileHash, false, _OldFileHash);
+_FileName, _FileType, block.timestamp, block.number,_FileHash, false, _OldFileHash);
 docHashes[_FileHash] = newTransmission;
 ListdocHash.push(_FileHash);
-
 }}
-
-
 }
-
 //Return transmission register function
 function Return_reg()
 public view
 returns (address[] memory, bytes32[] memory, bytes32[] memory, uint[] memory, 
 uint[] memory, bytes32[] memory, bool[] memory) {
+uint inum = ListdocHash.length;   
 //Initialisation of vectors 
-address[] memory Senders = new address[](ListdocHash.length);
-bytes32[] memory FileNames = new bytes32[](ListdocHash.length);
-bytes32[] memory DocTypes = new bytes32[](ListdocHash.length);
-uint[] memory mineTimes = new uint[](ListdocHash.length);
-uint[] memory blockNumbers = new uint[](ListdocHash.length);
-bytes32[] memory FileHashs = new bytes32[](ListdocHash.length);
-bool[] memory LstVers = new bool[](ListdocHash.length);
-
+address[] memory Senders = new address[](inum);
+bytes32[] memory FileNames = new bytes32[](inum);
+bytes32[] memory DocTypes = new bytes32[](inum);
+uint[] memory mineTimes = new uint[](inum);
+uint[] memory blockNumbers = new uint[](inum);
+bytes32[] memory FileHashs = new bytes32[](inum);
+bool[] memory LstVers = new bool[](inum);
 //Cycling through all the values I have on the hash list
-for (uint i = 0; i < ListdocHash.length; i++) {
+for (uint i = 0; i < inum ||i < 20; i++) {
 Senders[i]=docHashes[ListdocHash[i]].Sender;
 FileNames[i] = docHashes[ListdocHash[i]].FileName;
 DocTypes[i] = docHashes[ListdocHash[i]].DocType;
@@ -72,13 +63,10 @@ mineTimes[i] = docHashes[ListdocHash[i]].mineTime;
 blockNumbers[i] = docHashes[ListdocHash[i]].blockNumber;
 FileHashs[i] = docHashes[ListdocHash[i]].FileHash;
 LstVers[i] = docHashes[ListdocHash[i]].Current_version;
-
 }
 //Returning the Register of transmissions
 return (Senders, FileNames, DocTypes, mineTimes, blockNumbers, FileHashs,LstVers);
-
 }
-
 function Check_trans (bytes32 TdocHashes) public view returns(address,bytes32,
 bytes32,uint, uint,bytes32){
 if((docHashes[TdocHashes].Recipient == msg.sender ||
@@ -88,7 +76,5 @@ return
 (docHashes[TdocHashes].Sender,docHashes[TdocHashes].FileName,docHashes[TdocHashes].DocType, docHashes[TdocHashes].mineTime,
 docHashes[TdocHashes].blockNumber, docHashes[TdocHashes].FileHash);
 }
-
 }
-
 }
